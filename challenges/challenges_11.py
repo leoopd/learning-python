e8ee35530a05917d596ad6b1c9f4f7b753b8e589
Leo <109588159+leoopd@users.noreply.github.com>
2023-02-09 21:15:00 +0100

    Create challenges_11.py
    added challenges/challenges_11.py; milestone: chapter 10 done

---
+++ challenges/challenges_11.py
@@ -0,0 +1,6 @@
+# Challenge #1
+# Create a Python script that removes all the occurrences of a given element of a list.
+l = [1,2,2,3,2,4,5,2,6]
+n = int(input('Please enter a number to remove from the list: ')
+wile n in l:
+        l.remove(n)
