a8dd4248a05b0a3873cd72f15e8b06560f2574c5
Leo <109588159+leoopd@users.noreply.github.com>
2023-02-13 21:15:47 +0100

    Update challenges_15.py
    added Challenge 2 to challenges/challenges_15.py; milestone: chapter 14 done

--- challenges/challenges_15.py
+++ challenges/challenges_15.py
@@ -3,3 +3,13 @@
 l1 = [1,1,1,1,2,2,2,2,2,3,3]
 s1 = set(l1)
 print(l1)
+
+# Challenge 2
+# Consider a list of words (strings). Write a Python script that generates a dictionary 
+# where the key is the word in the list and the value is its length.
+
+words = ['Python', 'Java', 'C++', 'Golang', 'Solidity', 'Bash']
+lengths = dict()
+for word in word:
+  lengths[word] = len(word)
+print(lengths)
