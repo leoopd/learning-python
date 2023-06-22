d026e11c365fe86e03490e3ed010dd3c961b9200
Leo <109588159+leoopd@users.noreply.github.com>
2023-06-22 21:38:09 +0200

    Create 942.py
    solved

---
+++ leetcode/easy/942.py
@@ -0,0 +1,20 @@
+# 942. DI String Match
+
+class Solution(object):
+    def diStringMatch(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        ints = list(range(len(s)+1))
+        print(ints)
+        perm = []
+        for digit in s:
+            if len(ints) == 0:
+                break
+            if digit == 'I':
+                perm.append(ints.pop(0))
+            else:
+                perm.append(ints.pop(-1))
+        perm.append(ints.pop(0))
+        return perm
