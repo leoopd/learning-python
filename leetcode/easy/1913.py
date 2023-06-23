4d070f301c64237327c99b33affcf5756079ead5
Leo <109588159+leoopd@users.noreply.github.com>
2023-06-23 15:34:57 +0200

    Create 1913.py
    solved

---
+++ leetcode/easy/1913.py
@@ -0,0 +1,6 @@
+# 1913. Maximum Product Difference Between Two Pairs
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1]*nums[-2])-(nums[0]*nums[1])
