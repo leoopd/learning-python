73baecff4c1099fb2d74d85a97dbe2f6b16c2392
Leo <109588159+leoopd@users.noreply.github.com>
2023-06-30 21:26:20 +0200

    Create 1389.py
    solved

---
+++ leetcode/easy/1389.py
@@ -0,0 +1,13 @@
+# 1389. Create Target Array in the Given Order
+
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        target = []
+        for i in range(len(nums)):
+            target.insert(index[i], nums[i])
+        return target
