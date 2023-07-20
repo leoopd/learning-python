009ec49a61a342b1d985d7bf9fe1f65b36878170
Leo <109588159+leoopd@users.noreply.github.com>
2023-07-20 11:20:18 +0200

    Update 2544.py
    a little easier on memory

--- leetcode/easy/2544.py
+++ leetcode/easy/2544.py
@@ -3,13 +3,10 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
         digits = list(str(n))
-        switch = False
         sum = 0
         for i in range(len(digits)):
-            if switch == False:
+            if i%2 == 0:
                 sum += int(digits[i])
-                switch = True
             else:
                 sum -= int(digits[i])
-                switch = False
         return sum
