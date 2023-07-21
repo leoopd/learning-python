b1b3fe93a1783a1d1fdcf703d8728ea296bd6d30
Leo <109588159+leoopd@users.noreply.github.com>
2023-07-21 18:12:04 +0200

    Update 2423.py
    still not solved, because the smallest occurrance can also be removed

--- leetcode/easy/2423.py
+++ leetcode/easy/2423.py
@@ -2,18 +2,13 @@
 
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-        switch = False
         chars = set(list(word))
         counts = [word.count(x) for x in chars]
-        if counts[0] <= counts[-1]:
-            occ = counts[0]
-        else:
-            occ = counts[-1]
-        counts[counts.index(counts.max())] -= 1
-        print(counts)
-        for i in range(len(counts)):
-            if counts[i] == occ+1 and switch == False:
-                switch = True
-            elif counts[i] > occ:
+        biggest = max(counts)
+        if biggest != 1:
+            counts[counts.index(biggest)] -= 1
+
+        for i in range(1, len(counts)):
+            if counts[i] != counts[i-1]:
                 return False
         return True
