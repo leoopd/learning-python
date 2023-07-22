cf67d4850f50cf3e3a402abf627260a863d6602e
Leo <109588159+leoopd@users.noreply.github.com>
2023-07-22 18:35:59 +0200

    Update 2423.py
    hacky but finally works

--- leetcode/easy/2423.py
+++ leetcode/easy/2423.py
@@ -4,11 +4,30 @@ class Solution:
     def equalFrequency(self, word: str) -> bool:
         chars = set(list(word))
         counts = [word.count(x) for x in chars]
-        biggest = max(counts)
-        if biggest != 1:
-            counts[counts.index(biggest)] -= 1
 
-        for i in range(1, len(counts)):
-            if counts[i] != counts[i-1]:
+        least = counts.index(min(counts))
+        if min(counts) == 1:
+            option_one = counts[:least] + counts[least+1:]
+            print(option_one)
+        else:
+            option_one = counts.copy()
+            option_one[least] -= 1
+
+        if len(option_one) == 1:
+            return True
+
+        for i in range(1, len(option_one)):
+            if option_one[i-1] != option_one[i]:
+                break
+            if i == len(option_one)-1:
+                return True
+        
+        most = counts.index(max(counts))
+        option_two = counts.copy()
+        option_two[most] -= 1
+
+        for i in range(1, len(option_two)):
+            if option_two[i-1] != option_two[i]:
                 return False
-        return True
+            if i == len(option_two)-1:
+                return True
