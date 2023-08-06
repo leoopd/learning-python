58f1455afa8676fd958bb917852c90cea79f72fb
Leo <109588159+leoopd@users.noreply.github.com>
2023-08-06 20:25:19 +0200

    Create 1550.py
    solved

---
+++ leetcode/easy/1550/1550.py
@@ -0,0 +1,8 @@
+# 1550. Three Consecutive Odds
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(2, len(arr)):
+            if arr[i-2]%2 != 0 and arr[i-1]%2 != 0 and arr[i]%2 != 0:
+                return True
+        return False
