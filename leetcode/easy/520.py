cefcefa83e19ac7a529166838e69755b2c2a102c
Leo <109588159+leoopd@users.noreply.github.com>
2023-08-11 11:06:00 +0200

    Create 520.py
    solved

---
+++ leetcode/easy/520.py
@@ -0,0 +1,10 @@
+# 520. Detect Capital
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper():
+            return True
+        elif word.islower():
+            return True
+        elif word[0].isupper() and word[1:].islower():
+            return True
