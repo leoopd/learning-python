800bf09e7e2d6a298efb253b2cfe996355b1ada4
Leo <109588159+leoopd@users.noreply.github.com>
2023-08-19 21:09:22 +0200

    Update 2053.py
    faster but not fast

--- leetcode/easy/2053.py
+++ leetcode/easy/2053.py
@@ -3,9 +3,10 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         ctr = 0
-        for string in arr:
-            if arr.count(string) == 1:
-                ctr += 1
+        for i in range(len(arr)):
+            if arr[i] in arr[arr.index(arr[i])+1:]:
+                continue
+            ctr += 1
             if ctr == k:
-                return string
+                return arr[i]
         return ""
