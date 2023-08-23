005b9dc1a41b7eacc057767ee5949da1d19907f8
Leo <109588159+leoopd@users.noreply.github.com>
2023-08-23 20:17:50 +0200

    Update 1408.py
    easier to read and a little faster

--- leetcode/easy/1408.py
+++ leetcode/easy/1408.py
@@ -3,10 +3,11 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         res = []
-        for i in range(len(words)):
-            tmp = words[:i] + words[i+1:]
-            for j in range(len(tmp)):
-                if words[i] in tmp[j]:
-                    res.append(words[i])
+        for word1 in words:
+            for word2 in words:
+                if word1 == word2:
+                    continue
+                if word1 in word2:
+                    res.append(word1)
                     break
         return res
